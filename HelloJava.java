e309c59097c3b823d49c992575d20dd2175ecc1a
JunHyeon Mun <impelfin@gmail.com>
2017-09-01 02:03:41 +0900

    Update HelloJava.java

--- HelloJava.java
+++ HelloJava.java
@@ -1,5 +1,6 @@
 class HelloJava {
 	public static void main(String[] args) {
-		System.out.println("Hello Java~!! ");
+		System.out.println("Hello Java~!!");
+		System.out.println("Test Test");
 	}
 }
